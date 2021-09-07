49cccd4e375ac4b1f2d5452cf0f6e2b59cf51a81
Riley Kanko <go4ashes2.0@gmail.com>
2021-09-07 10:13:06 -0700

    done

--- prime.rb
+++ prime.rb
@@ -1,1 +1,19 @@
-# Add  code here!
+def prime?(num)
+    if num >= 2
+        (2..num - 1).all? do |n|
+            num % n != 0
+        end
+    else
+        return false
+    end
+end
+
+
+#def prime?(num)
+    #if num >= 2
+        #results = (2..num - 1).to_a.map {|n| num % n == 0}
+        #!results.include?(true)
+    #else
+        #return false
+    #end
+end
